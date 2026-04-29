652afd6e7d0bfafcb8f192a36f8db213bd42f75d
Felipe <felipeerebollo@gmail.com>
2026-04-28 21:25:02 -0300

    Update Tester.java

--- src/dominio/Tester.java
+++ src/dominio/Tester.java
@@ -1,3 +1,47 @@
-private String nombre;
-private int edad;
-private int experiencia;
+public class Tester {
+  private String nombre;
+  private int edad;
+  private int experiencia;
+
+  public String getNombre() {
+      return nombre;
+  }
+  public void setNombre(String unNombre){
+      nombre = unNombre;
+  }
+
+  public int getEdad(){
+      return edad;
+  }
+  public void setEdad(int unaEdad){
+      edad = unaEdad
+  }
+
+  public int Experiencia(){
+      return edad;
+  }
+  public void setExperiencia(int unaExperiencia){
+      experiencia = unaExpriencia
+  }
+
+  public Tester(String unNombre, int unaEdad, int unaExperiencia) {
+      this.setNombre(unNombre);
+      this.setEdad(unaEdad);
+      this.setExperiencia(unaExperiencia);
+  }
+
+  public Tester() {
+      this.setNombre("Sin nombre")
+      this.setEdad(18);
+      this.setExperiencia(0);
+  }
+
+
+  @Override
+  public String toString() {
+      return this.getNombre() + " - Edad: " + this.getEdad() + " - Experiencia:"
+  }
+  
+
+  
+}
