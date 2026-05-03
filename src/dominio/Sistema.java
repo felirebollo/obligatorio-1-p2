fa26089afe586d98a0387e7640b4cbf58b76976f
Felirebollo <felipeerebollo@gmail.com>
2026-05-02 22:04:29 -0300

    mm

--- src/dominio/Sistema.java
+++ src/dominio/Sistema.java
@@ -3,11 +3,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dominio;
-
+import java.util.ArrayList;
 /**
  *
  * @author felipe
  */
 public class Sistema {
+    private ArrayList<Tester> listaTesters;
     
+    public Sistema(){
+        listaTesters = new ArrayList<Tester>();
+    }
+    
+    public ArrayList<Tester> getListaTesters() {
+        return listaTesters;
+    }
+    
+    public boolean existeTester(String unNombre){
+        boolean existe = false;
+        
+        int i = 0;
+        while(i < this.getListaTesters().size()){
+            Tester aux = this.getListaTesters().get(i);
+            
+            if(aux.getNombre().equals(unNombre)) {
+                existe = true;
+            }
+            i=i+1;
+        }
+        return existe;
+    }
+    
+    public void agregarTester(Tester unTester) {
+        if(!this.existeTester(unTester.getNombre())) {
+            this.getListaTesters().add(unTester);
+        }
+    }
 }
