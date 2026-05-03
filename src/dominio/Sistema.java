fe4cad392dba15881903b2890b78f55ae8b21516
Felipe <felipeerebollo@gmail.com>
2026-05-03 17:41:45 -0300

    agregar testeo al sistema y retornar lista de testeos

--- src/dominio/Sistema.java
+++ src/dominio/Sistema.java
@@ -5,9 +5,8 @@ import java.util.ArrayList;
 // ULTIMO PUSH 03/05 A LAS 15:13
 
 public class Sistema {
-     private ArrayList <Tester> listaTesters = new ArrayList <>();  
-       
-    
+     private ArrayList<Tester> listaTesters = new ArrayList<>();  
+     private ArrayList<Testeo> listaTesteos = new ArrayList<>();
     
     public void agregarTester (Tester nuevoTester){
        listaTesters.add(nuevoTester);
@@ -29,7 +28,17 @@ public class Sistema {
     public ArrayList<Tester> getListaTester(){ 
          return listaTesters;
     }
-    
+
+
+     // Agrega un testeo realizado al sistema
+     public void agregarTesteo(Testeo unTesteo) {
+         listaTesteos.add(unTesteo);
+     }
+     
+     // Retorna la lista de testeos realizados
+     public ArrayList<Testeo> getListaTesteos() {
+          return listaTesteos;
+     }
     
     
     
