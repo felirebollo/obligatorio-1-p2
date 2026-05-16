be48dfa7a715e11d2edf837105601780ac5d38d4
Felirebollo <felipeerebollo@gmail.com>
2026-05-16 18:21:28 -0300

    ordenado

--- src/dominio/Sistema.java
+++ src/dominio/Sistema.java
@@ -7,77 +7,74 @@ import java.util.ArrayList;
 // ULTIMO Push 04/05 A LAS 20:27
 // ULTIMO Push 05/05 A LAS 09:25
 // ULTIMO Push 05/05 A LAS 11:32
-
-
 public class Sistema {
-     private ArrayList<Tester> listaTesters = new ArrayList<>();  
-     private ArrayList<Testeo> listaTesteos = new ArrayList<>();
-     private Tablero ultimoTablero = new Tablero ();
 
-     
-     public void setUltimoTablero (Tablero unTablero)
-      {ultimoTablero = unTablero;}
-     
-     public Tablero getUltimoTablero ()
-      {
-       return ultimoTablero;
-      }
-     
-     // Guarda el proximo numero de testeo
-     private int proximoNumeroTesteo = 1;
-     
-     public void agregarTester (Tester nuevoTester){
-          listaTesters.add(nuevoTester);
-     }
-    
-    // Detecta si el nombre ingresado para el tester ya existe 
-    public boolean nombreRepetido (String nuevoNombre){
-     boolean repetido = false;
-       
-       for (int i = 0 ; i < listaTesters.size() ; i++){
-          String nombre = listaTesters.get(i).getNombre();
-          if (nombre.equalsIgnoreCase(nuevoNombre)){
-               repetido = true;
-          }
-              
-        } 
-       return repetido;
-     }
-    
-    public ArrayList<Tester> getListaTester(){ 
-         return listaTesters;
+    private ArrayList<Tester> listaTesters = new ArrayList<>();
+    private ArrayList<Testeo> listaTesteos = new ArrayList<>();
+    private Tablero ultimoTablero = new Tablero();
+
+    public void setUltimoTablero(Tablero unTablero) {
+        ultimoTablero = unTablero;
     }
 
+    public Tablero getUltimoTablero() {
+        return ultimoTablero;
+    }
 
-     // Agrega un testeo realizado al sistema
-     public void agregarTesteo(Testeo unTesteo) {
-         listaTesteos.add(unTesteo);
-     }
-     
-     // Retorna la lista de testeos realizados
-     public ArrayList<Testeo> getListaTesteos() {
-          return listaTesteos;
-     }
+    // Guarda el proximo numero de testeo
+    private int proximoNumeroTesteo = 1;
 
-     // Devuelve el proximo numero de testeo y lo incrementa
-     public int obtenerProximoNumeroTesteo() {
-     int numero = proximoNumeroTesteo;
-     proximoNumeroTesteo = proximoNumeroTesteo + 1;
-     return numero;
-}
-     public boolean existeTester (String nombre)
-      {
-         boolean existe = false;
-         
-         for (int i = 0 ; i < this.getListaTester().size() ; i++)
-         {
-                if(this.getListaTester().get(i).getNombre().equalsIgnoreCase(nombre)){existe = true;}
-         }
-         return existe;
-      }
-     
-        
-   /* 
+    public void agregarTester(Tester nuevoTester) {
+        listaTesters.add(nuevoTester);
+    }
+
+    // Detecta si el nombre ingresado para el tester ya existe 
+    public boolean nombreRepetido(String nuevoNombre) {
+        boolean repetido = false;
+
+        for (int i = 0; i < listaTesters.size(); i++) {
+            String nombre = listaTesters.get(i).getNombre();
+            if (nombre.equalsIgnoreCase(nuevoNombre)) {
+                repetido = true;
+            }
+
+        }
+        return repetido;
+    }
+
+    public ArrayList<Tester> getListaTester() {
+        return listaTesters;
+    }
+
+    // Agrega un testeo realizado al sistema
+    public void agregarTesteo(Testeo unTesteo) {
+        listaTesteos.add(unTesteo);
+    }
+
+    // Retorna la lista de testeos realizados
+    public ArrayList<Testeo> getListaTesteos() {
+        return listaTesteos;
+    }
+
+    // Devuelve el proximo numero de testeo y lo incrementa
+    public int obtenerProximoNumeroTesteo() {
+        int numero = proximoNumeroTesteo;
+        proximoNumeroTesteo = proximoNumeroTesteo + 1;
+        return numero;
+    }
+
+    public boolean existeTester(String nombre) {
+        boolean existe = false;
+
+        for (int i = 0; i < this.getListaTester().size(); i++) {
+            if (this.getListaTester().get(i).getNombre().equalsIgnoreCase(nombre)) {
+                existe = true;
+            }
+        }
+        return existe;
+    }
+
+    /* 
      VER PARA BORRRAR
      public Tablero getUltimoTablero ()
      {
@@ -91,10 +88,5 @@ public class Sistema {
        
         return salida;
      }
-    */ 
-     
-   }
-
-
-
-
+     */
+}
